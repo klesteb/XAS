07bfdafce93c2b2993a09cc21c1744e5ad7400c3
kesteb <kesteb@21246f32-d8fa-4066-b0f1-931d2724148b>
2014-05-28 22:16:20 +0000

    Added test scripts
    git-svn-id: http://svn.kesteb.us/repos/XAS@9 21246f32-d8fa-4066-b0f1-931d2724148b

---
+++ trunk/test/test-log-file.pl
@@ -0,0 +1,18 @@
+use lib '../lib';
+use XAS::Factory;
+
+my $log = XAS::Factory->module('log', { 
+   -type     => 'file',
+   -filename => 'test.log',
+   -levels => {
+       trace => 1
+   }
+});
+
+$log->info('it works');
+$log->level('debug', 1);
+printf("debug = %s\n", $log->level('debug'));
+$log->debug('debug works');
+$log->trace('trace works');
+$log->info("this is", "a test");
+    
---
+++ trunk/test/test-log.pl
@@ -0,0 +1,12 @@
+use lib '../lib';
+use XAS::Factory;
+
+my $log = XAS::Factory->module('log');
+
+$log->info('it works');
+$log->level('debug', 1);
+printf("debug = %s\n", $log->level('debug'));
+$log->debug('debug works');
+$log->trace('trace works');
+$log->info("this is", "a test");
+    
