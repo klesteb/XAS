63d228d6e398f11df1ed2b751640d7f0f34d93c5
Kevin L. Esteb <kevin@kesteb.us>
2014-09-19 15:55:41 -0700

    Added more tests

--- trunk/test/test-pubsub.pl
+++ trunk/test/test-pubsub.pl
@@ -125,7 +125,82 @@ sub session_idle {
     $self->events->publish(
         -event   => 'testing', 
         -channel => 'system',
-        -args    => ['test2', 'test2']
+        -args => {
+            test1 => 'test2', 
+            test2 => 'test2'
+        }
+    );
+
+    $self->SUPER::session_idle();
+    
+}
+
+sub testing {
+    my ($self, $args) = @_[OBJECT, ARG0];
+
+    my $alias = $self->alias;
+
+    $self->log->debug("$alias: testing()");
+
+    warn Dumper($args);
+
+}
+
+sub init {
+    my $class = shift;
+
+    my $self = $class->SUPER::init(@_);
+
+    $self->{events} = XAS::Lib::POE::PubSub->new();
+
+    return $self;
+
+}
+
+package Test3;
+
+use POE;
+use XAS::Lib::POE::PubSub;
+use XAS::Class
+  debug     => 0,
+  version   => '0.01',
+  base      => 'XAS::Lib::POE::Service',
+  accessors => 'events',
+  vars => {
+    PARAMS => {
+      -alias => { optional => 1, default => 'test2' }
+    }
+  }
+;
+
+use Data::Dumper;
+
+sub session_initialize {
+    my $self = shift;
+
+    my $alias = $self->alias;
+
+    $self->log->debug("$alias: session_initialize");
+
+    $self->events->subscribe($alias, 'system');
+
+    $poe_kernel->state('testing', $self);
+
+    $self->SUPER::session_initialize();
+
+}
+
+sub session_idle {
+    my $self = $_[OBJECT];
+
+    my $alias = $self->alias;
+
+    $self->log->debug("$alias: runner");
+
+    $self->events->publish(
+        -event   => 'testing', 
+        -channel => 'system',
+        -args   => 'this is a test'
     );
 
     $self->SUPER::session_idle();
@@ -171,8 +246,9 @@ sub main {
     my $service = XAS::Lib::Services->new();
     my $test1 = Test1->new(-alias => 'test1');
     my $test2 = Test2->new(-alias => 'test2');
+    my $test3 = Test3->new(-alias => 'test3');
 
-    $service->register('test1 test2');
+    $service->register('test1 test2 test3');
     $service->run();
 
     $self->log->info('stop run');
