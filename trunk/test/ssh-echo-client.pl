d4e6db9b3a5c124f207bcb45e81a208b53c51f4b
root <root@bob.kesteb.us>
2014-09-26 14:07:10 -0700

    Added a sample client/server for SSH

---
+++ trunk/test/ssh-echo-client.pl
@@ -0,0 +1,34 @@
+use lib '../lib';
+
+use Data::Dumper;
+use XAS::Lib::SSH::Client::Shell;
+
+my $command = 'dev/XAS/trunk/test/ssh-echo-server';
+my $shell = XAS::Lib::SSH::Client::Shell->new(
+    -server   => 'localhost',
+    -username => 'kevin',
+    -priv_key => '/home/kevin/.ssh/id_rsa',
+    -pub_key  => '/home/kevin/.ssh/id_rsa.pub',
+);
+
+$shell->connect();
+
+$shell->run($command);
+
+$shell->call("this is a test", sub {
+    my $output = shift;
+    printf("%s\n", $output);
+});
+
+$shell->call("this is another test", sub {
+    my $output = shift;
+    printf("%s\n", $output);
+});
+
+$shell->call("this is another test again", sub {
+    my $output = shift;
+    printf("%s\n", $output);
+});
+
+$shell->disconnect();
+
