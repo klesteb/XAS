1a2f0b561598a55113a1cd7191edd318200f1914
Kevin L. Esteb <kevin@kesteb.us>
2014-10-03 08:19:15 -0700

    Added a forgotten file

---
+++ trunk/test/ssh-exec.pl
@@ -0,0 +1,26 @@
+use lib '../lib';
+
+use XAS::Lib::SSH::Client::Exec;
+
+my $shell = XAS::Lib::SSH::Client::Exec->new(
+    -server   => 'localhost',
+    -username => 'kevin',
+    -priv_key => '/home/kevin/.ssh/id_rsa',
+    -pub_key  => '/home/kevin/.ssh/id_rsa.pub',
+);
+
+$shell->connect();
+
+$shell->call("ls -la", sub {
+    my $output = shift;
+
+    foreach my $line (@$output) {
+
+        printf("%s\n", $line);
+
+    }
+
+});
+
+$shell->disconnect();
+
